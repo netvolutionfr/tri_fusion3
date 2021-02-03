e7d3a326d7520608bba7a3350452f7f4d8a5adfa
shumez <stanis@netvolution.fr>
2021-02-03 14:22:16 +0100

    Fin de l'algo tri fusion dichotomique (récursif)

--- trifusion.py
+++ trifusion.py
@@ -1,13 +1,3 @@
-def tri_fusion(l):
-    longueur = len(l)
-    if longueur>1:
-        return tri_fusion(l[])
-    t = l
-    return t
-
-
-# assert tri_fusion([38, 27, 43, 3, 9, 72, 10]) == [3, 9, 10, 27, 38, 43, 82]
-
 def fusion(l1, l2):
     i1 = 0
     i2 = 0
@@ -31,3 +21,15 @@ def fusion(l1, l2):
 assert fusion([35, 56], [32, 42]) == [32, 35, 42, 56]
 assert fusion([35, 56, 68, 89], [1, 32, 42]) == [1, 32, 35, 42, 56, 68, 89]
 
+def tri_fusion(l):
+    longueur = len(l)
+    if longueur>1:
+        milieu = longueur // 2
+        return fusion(tri_fusion(l[:milieu]), tri_fusion(l[milieu:]))
+    else:
+        t = l
+    return t
+
+
+assert tri_fusion([38, 27, 43, 3, 9, 72, 10]) == [3, 9, 10, 27, 38, 43, 72]
+assert tri_fusion([91, 4, 52, 14, 12, 1, 9, 66, 13]) == [1, 4, 9, 12, 13, 14, 52, 66, 91]
